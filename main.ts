0ec1bd18c4ea3ce4501d1e52e4cb5d1c833ec563
jamesg164 <84445485+jamesg164@users.noreply.github.com>
2021-05-19 11:16:31 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,374 @@
+namespace SpriteKind {
+    export const player2 = SpriteKind.create()
+    export const projectile2 = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.x < mySprite2.x && mySprite.y < mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, 100)
+    } else if (mySprite.x > mySprite2.x && mySprite.y > mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -100, -100)
+    } else if (mySprite.x > mySprite2.x && mySprite.y < mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -100, 100)
+    } else if (mySprite.x < mySprite2.x && mySprite.y > mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, -100)
+    } else if (mySprite.x == mySprite2.x && mySprite.y > mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 0, 100)
+    } else if (mySprite.x == mySprite2.x && mySprite.y < mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 0, -100)
+    } else if (mySprite.x < mySprite2.x && mySprite.y == mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 100, 0)
+    } else if (mySprite.x > mySprite2.x && mySprite.y == mySprite2.y) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . 8 8 8 8 8 8 . . . . . 
+            . . . . . . 8 8 8 8 . . . . . . 
+            . . . . . . . 8 8 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, -100, 0)
+    }
+    projectile.setKind(SpriteKind.Projectile)
+})
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (mySprite2.x < mySprite.x && mySprite2.y < mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 100, 100)
+    } else if (mySprite2.x > mySprite.x && mySprite2.y > mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, -100, -100)
+    } else if (mySprite2.x > mySprite.x && mySprite2.y < mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, -100, 100)
+    } else if (mySprite2.x < mySprite.x && mySprite2.y > mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 100, -100)
+    } else if (mySprite2.x == mySprite.x && mySprite2.y > mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 0, 100)
+    } else if (mySprite2.x == mySprite.x && mySprite2.y < mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 0, -100)
+    } else if (mySprite2.x < mySprite.x && mySprite2.y == mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, 100, 0)
+    } else if (mySprite2.x > mySprite.x && mySprite2.y == mySprite.y) {
+        Projectile2 = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            . . . . . . 2 2 2 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite2, -100, 0)
+    }
+    Projectile2.setKind(SpriteKind.projectile2)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.projectile2, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.player2, SpriteKind.Projectile, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.player2.changeLifeBy(-1)
+})
+let Projectile2: Sprite = null
+let projectile: Sprite = null
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(13)
+game.showLongText("Welcome to the two player game where you don't have to aim", DialogLayout.Bottom)
+mySprite = sprites.create(img`
+    . . . . . f 8 8 8 8 f . . . . . 
+    . . . . . 8 8 8 8 8 8 . . . . . 
+    . . . . . 8 8 8 8 8 8 . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . 8 8 8 8 8 8 . . . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+    . . . 8 8 8 8 8 8 8 8 8 8 . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . . . . . 8 8 . . . . . . . 
+    . . . . . . 8 8 8 8 . . . . . . 
+    . . . . . . 8 . . 8 . . . . . . 
+    . . . . 8 8 . . . . 8 8 . . . . 
+    . . . 8 . . . . . . . . 8 . . . 
+    . . . 8 . . . . . . . . 8 . . . 
+    `, SpriteKind.Player)
+mySprite2 = sprites.create(img`
+    . . . . . f 2 2 2 2 f . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . 2 2 2 2 2 2 . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 2 . . . . . . 
+    . . . . . . 2 . . 2 . . . . . . 
+    . . . . 2 2 . . . . 2 2 . . . . 
+    . . . 2 . . . . . . . . 2 . . . 
+    . . . 2 . . . . . . . . 2 . . . 
+    `, SpriteKind.player2)
+controller.moveSprite(mySprite)
+controller.player2.moveSprite(mySprite2)
+info.player1.setLife(3)
+info.player2.setLife(3)
+mySprite.setFlag(SpriteFlag.StayInScreen, true)
+mySprite2.setFlag(SpriteFlag.StayInScreen, true)
+mySprite.setPosition(randint(0, 160), randint(0, 120))
+mySprite2.setPosition(randint(0, 160), randint(0, 120))
